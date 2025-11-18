56f72bbb3bf2057487db0942e9a3de50d7089b69
Thomas Rife <112517908+thomas-rife@users.noreply.github.com>
2025-11-17 16:52:05 -0800

    adding updates for deployment

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
-import app from './src/index.js'
+// import app from './src/index.js'
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`)
+// })
